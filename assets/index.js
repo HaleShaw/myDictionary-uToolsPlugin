4c4ba1b895b77743ea16a1ec639eabf3fbb0fb4c
Hale Shaw <hongliang.xiao@sap.com>
2021-01-26 13:27:56 +0800

    look up the word immediately

--- assets/index.js
+++ assets/index.js
@@ -1,95 +1,147 @@
-var text = ''
-var t
+var maxHeight = 550;
+var youDaoApi = 'https://m.youdao.com/dict?le=eng&q=';
+
 utools.onPluginEnter(({ code, type, payload }) => {
     utools.setExpendHeight(0);
-    if(code == 'search_word'){
-        utools.setSubInput(({text}) => {
-            this.text = text
-        }, "请输入需要查询的中英文词汇");
-        if(type=='over'){
+    if (code == 'search_word') {
+        utools.setSubInput(({ text }) => {
+            lookUp(text);
+        }, '请输入需要查询的中英文内容');
+        if (type == 'over') {
             utools.setSubInputValue(payload);
-            search_word(payload);
+            lookUp(payload);
         }
-       
     }
 });
 
 $(document).keydown(e => {
     switch (e.keyCode) {
         case 13:
-            search_word(text)
+            console.log(text);
+            lookUp(text);
             break;
     }
 });
 
-
-function search_word(word){
-    if(word==''){
+async function lookUp(word) {
+    let contentFather = $('.content');
+    word = word.trim();
+    if (word == '') {
+        contentFather.html('');
+        utools.setExpendHeight(0);
         return;
     }
-    utools.setExpendHeight(544);
-    $(".content").html('');
-    var append_html = ""
-    var url = "https://m.youdao.com/dict?le=eng&q="+word;
-    $.get(url, function(data){
-        t=data
-        append_html = '<h1>'+word+'</h1>';
+    let url = youDaoApi + word;
+    $.get(url, function (data) {
+        contentFather.html('');
+        let title = getTitle(word);
+        contentFather.append(title);
 
-        //读音 英/美
-        var reg_audio = /英[\W\w]*?phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"[\W\w]*?美[\W\w]*?phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"/im;
-        var str_audio = reg_audio.exec(data)
-       
-        if(str_audio != null){
-            //append_html += '<h3 class="phonetic">英'+str_audio[1]+'<button class="play-phonetic-btn" type="button" onclick="playVoice(\''+str_audio[2]+'\');"></button>&nbsp;&nbsp;&nbsp;美'+str_audio[3]+'<button class="play-phonetic-btn" type="button" onclick="playVoice(\''+str_audio[4]+'\');"></button></h3>';
-            append_html += '<h3 class="phonetic">英'+str_audio[1]
-            +'<audio controls="controls" id="audio_player_1" style="display:none;" src="'+str_audio[2]+'"></audio>'
-            +'<button class="play-phonetic-btn" type="button" onclick="playVoice(\'audio_player_1\')"></button>&nbsp;&nbsp;&nbsp;美'+str_audio[3]
-            +'<audio controls="controls" id="audio_player_2" style="display:none;" src="'+str_audio[4]+'"></audio>'
-            +'<button class="play-phonetic-btn" type="button" onclick="playVoice(\'audio_player_2\')"></button></h3>';
-        }else{
-            //读音 单个读音
-            var reg_audio = /phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"/im;
-            var str_audio = reg_audio.exec(data)
-            if(str_audio != null){
-                console.log(str_audio)
-                append_html += '<h3 class="phonetic">'+str_audio[1]+'<button class="play-phonetic-btn" type="button" onclick="playVoice(\''+str_audio[2]+'\');"></button></h3>';
-            }
+        let phoneticDiv = getPhonetic(data);
+        contentFather.append(phoneticDiv);
 
-        }
+        let paraphraseDiv = getParaphrase(data);
+        contentFather.append(paraphraseDiv);
 
-        //基础解释
-        var reg_ecContentWrp = /_contentWrp"[\W\w]*<ul>([\W\w]*?)<\/ul/im;
-        var str_ecCcontentWrp = reg_ecContentWrp.exec(data)
-        if(str_ecCcontentWrp != null){
-            append_html  += '<h2>释义</h2><ul>'+filterATag(str_ecCcontentWrp[1])+'</ul>';       
-        }
-
-        //翻译
-        var reg_fanyiContentWrp = /fanyi_contentWrp"[\W\w]*?翻译结果[\W\w]*?trans-container[\W\w]*?<p>[\W\w]*?<\/p>([\W\w]*?)<\/p>/im;
-        var str_fanyiContentWrp = reg_fanyiContentWrp.exec(data)
-        if(str_fanyiContentWrp != null){
-            append_html  += '<h2>翻译</h2><ul><li>'+str_fanyiContentWrp[1]+'</li></ul>';       
-        }
-
-        // $.each(data.items, function(i,item){
-        //      append_html  += "<li><img onmouseenter=\"bigImg(this)\" src='"+item.picUrl+"' onerror=\"this.onerror='';src='assets/loading.gif'\" /></li>";
-        //   });
-        $(".content").append(append_html);
-        setTimeout(function(){ loading = false}, 1000);
+        let translationDiv = getTranslation(data);
+        contentFather.append(translationDiv);
+        setHeight();
     });
+}
+
+function getTitle(word) {
+    return $('<h1>' + word + '</h1>');
+}
+
+function getPhonetic(data) {
+    //读音 英/美
+    let regPhonetic = /英[\W\w]*?phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"[\W\w]*?美[\W\w]*?phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"/im;
+    let phonetics = regPhonetic.exec(data);
+
+    let phoneticDiv = $('<div class="phonetic"></div>');
+
+    // 多个读音
+    if (phonetics != null) {
+        let keyEnSpan = $('<span>英 ' + phonetics[1] + '</span>');
+        let keyUtSpan = $('<span>美 ' + phonetics[3] + '</span>');
+
+        let audioEn = $('<audio></audio>');
+        audioEn.attr('src', phonetics[2]);
+        let audioUt = $('<audio></audio>');
+        audioUt.attr('src', phonetics[4]);
+
+        let btnEn = $('<button type="button"></button>');
+        btnEn.append(audioEn);
+        let btnUt = $('<button type="button"></button>');
+        btnUt.append(audioUt);
+
+        addListener(btnEn);
+        addListener(btnUt);
+        phoneticDiv.append(keyEnSpan);
+        phoneticDiv.append(btnEn);
+        phoneticDiv.append(keyUtSpan);
+        phoneticDiv.append(btnUt);
+    } else {
+        // 单个读音
+        regPhonetic = /phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"/im;
+        phonetics = regPhonetic.exec(data);
+        if (phonetics != null) {
+            console.log(phonetics);
+            let keySpan = $('<span>' + phonetics[1] + '</span>');
+            let audio = $('<audio></audio>');
+            audio.attr('src', phonetics[2]);
+            let btn = $('<button type="button"></button>');
+            btn.append(audio);
+            addListener(btn);
+            phoneticDiv.append(keySpan);
+            phoneticDiv.append(btn);
+        }
+    }
+    return phoneticDiv;
+}
+
+function addListener(element) {
+    // Convert to HTML DOM.
+    let btn = element[0];
+    btn.onmouseover = function () {
+        btn.children[0].play();
+    };
+}
+
+function getParaphrase(data) {
+    const regParaphrase = /_contentWrp"[\W\w]*<ul>([\W\w]*?)<\/ul/im;
+    let paraphrases = regParaphrase.exec(data);
+    let paraphraseDiv = $('<div></div>');
+    if (paraphrases != null) {
+        let paraphraseTitle = $('<h2>释义</h2>');
+        let paraphraseList = $('<ul>' + filterATag(paraphrases[1]) + '</ul>');
+        paraphraseDiv.append(paraphraseTitle);
+        paraphraseDiv.append(paraphraseList);
+    }
+    return paraphraseDiv;
 }
 
 function filterATag(msg) {
     var msg = msg.replace(/<a[\W\w]*?>/gim, '');
-    msg = msg.replace('</a>', '')
+    msg = msg.replace('</a>', '');
     return msg;
 }
 
-
-function playVoice2(file) {
-    $('#voice').html('<audio controls="controls" id="audio_player" style="display:none;"> <source src="' + file + '" > </audio><embed id="MPlayer_Alert" src="' + file + '" loop="false" width="0px" height="0px" /></embed>');
+function getTranslation(data) {
+    const regTranslation = /fanyi_contentWrp"[\W\w]*?翻译结果[\W\w]*?trans-container[\W\w]*?<p>[\W\w]*?<\/p>([\W\w]*?)<\/p>/im;
+    let translations = regTranslation.exec(data);
+    let translationDiv = $('<div></div>');
+    if (translations != null) {
+        let translationTitle = $('<h2>翻译</h2>');
+        let translationList = $('<ul><li>' + translations[1] + '</li></ul>');
+        translationDiv.append(translationTitle);
+        translationDiv.append(translationList);
+    }
+    return translationDiv;
 }
 
-function playVoice(id_name){
-    document.getElementById(id_name).play();
+function setHeight() {
+    let height = document.body.offsetHeight;
+    height = height > maxHeight ? maxHeight : height;
+    utools.setExpendHeight(height);
 }
